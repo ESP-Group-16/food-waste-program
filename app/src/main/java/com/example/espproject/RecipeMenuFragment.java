7f6f0bf81fcd7c6e79713b7e2b979139b717c47b
ionaclowes <iona.clowes@gmail.com>
2023-02-20 17:00:50 +0000

    added to nav

--- app/src/main/java/com/example/espproject/RecipeMenuFragment.java
+++ app/src/main/java/com/example/espproject/RecipeMenuFragment.java
@@ -6,14 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+//import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.espproject.databinding.FragmentFirstBinding;
+//import com.example.espproject.databinding.FragmentFirstBinding;
+
 public class RecipeMenuFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+
+
 }
