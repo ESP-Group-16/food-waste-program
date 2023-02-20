3d03c89dcd1e5242fa9f99032454756dc2573ccd
farazovo <farazovo@gmail.com>
2023-02-20 16:20:49 +0000

    partial connect jav to xml

--- app/src/main/java/com/example/espproject/RecipeMenuFragment.java
+++ app/src/main/java/com/example/espproject/RecipeMenuFragment.java
@@ -8,15 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.espproject.databinding.FragmentFirstBinding;
 public class RecipeMenuFragment extends Fragment {
-
-    // TODO: declare UI components as fields
-
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
