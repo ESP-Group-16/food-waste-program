50aba45fe5d298f812598b5c350ba56e7f7d2547
Faraz Khalid <farazovo@gmail.com>
2023-02-21 07:28:39 +0000

    recipe menu to recipie nav WORKING
    now that works just gotta copy to every page

--- app/src/main/java/com/example/espproject/MainActivity.java
+++ app/src/main/java/com/example/espproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.espproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.example.espproject.databinding.ActivityMainBinding;
+
+
 // main login page
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private TextView txt;
     private String TAG = "MainActivity";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         //txt = findViewById(R.id.textView);
 
@@ -39,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-
+            ;
         }
         //createAccount();
     }
--- app/src/main/java/com/example/espproject/RecipeMenuFragment.java
+++ app/src/main/java/com/example/espproject/RecipeMenuFragment.java
@@ -8,12 +8,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.espproject.databinding.FragmentRecipeMenuBinding;
 
 public class RecipeMenuFragment extends Fragment {
+
+    private FragmentRecipeMenuBinding binding;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRecipeMenuBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonPantry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(RecipeMenuFragment.this)
+                        .navigate(R.id.action_recipeMenuFragment_to_recipeFragment);
+            }
+        });
     }
 }
 
