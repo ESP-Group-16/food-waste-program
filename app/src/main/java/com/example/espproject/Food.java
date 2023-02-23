2930f1c6c9928eeb87075a41007c7bf811502d03
ekc30 <100845065+ekc30@users.noreply.github.com>
2023-02-23 09:34:48 +0000

    ESPG16-91

--- app/src/main/java/com/example/espproject/Food.java
+++ app/src/main/java/com/example/espproject/Food.java
@@ -3,4 +3,5 @@ package com.example.espproject;
 
 public class Food {
 
+
 }
--- app/src/main/java/com/example/espproject/PantryActivity.java
+++ app/src/main/java/com/example/espproject/PantryActivity.java
@@ -1,12 +1,32 @@
 package com.example.espproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PantryActivity extends AppCompatActivity implements AddIngredientDialog.AddItem, CreatePantryDialog.CreateNewPantry {
+    
+    private String TAG = "PantryActivity";
+    private FirebaseAuth mAuth;
+    private Map<String, Object> pantryMap = new HashMap<>();
 
     // CreatePantryDialog passes the name of the new pantry using this method
     @Override
@@ -22,7 +42,7 @@ public class PantryActivity extends AppCompatActivity implements AddIngredientDi
 
     // TODO: declare UI components as fields
 
-
+    private TextView txtPantryText;
 
     private FirebaseFirestore firebaseFirestore;
     private Pantry pantry;
@@ -32,14 +52,42 @@ public class PantryActivity extends AppCompatActivity implements AddIngredientDi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry);
 
+        txtPantryText = findViewById(R.id.txtPantryText);
+
         //TODO: review where to initialize this in the future
         firebaseFirestore = FirebaseFirestore.getInstance();
 
-        //retrieve pantry from the database
-        pantry = pantry.retrievePantry(firebaseFirestore);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
         // TODO: check if user has a pantry, start PantryFragment or NoPantryFragment accordingly
+        
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+        if(email != null) {
+            txtPantryText.setText("Hello " + email + "!");
+        }
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        //retrieve pantry from the database
+        db.collection("pantries")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                pantryMap = document.getData();
+                                txtPantryText.setText(pantryMap.get("name").toString());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
     }
 }
